7aa97b2e796ea10432d231fad5fc1d809ee2e5d8
mor19-meet <mor19@meet.mit.edu>
2017-08-09 17:08:37 +0300

    Stopping here

--- orrmor.py
+++ orrmor.py
@@ -12,7 +12,8 @@ WINDOW_SIZE_Y = 600
 turtle.setup(WINDOW_SIZE_X , WINDOW_SIZE_Y)
 turtle.bgpic('sea1.gif')
 foodpos_list = []
-
+var=0
+SPACEBAR = "space"
 #the edges of the screen
 
 UP_EDGE = WINDOW_SIZE_Y/2
@@ -37,7 +38,10 @@ turtle_drops = turtle.clone()
 turtle_drops.hideturtle()
 turtle_drops.goto(-WINDOW_SIZE_X/2+10,-WINDOW_SIZE_Y/2+10)
 turtle_drops.write('drops: '+str(m), font=('Arial',30,('bold','italic')))
-
+turtle_story=turtle.clone()
+turtle_story.hideturtle()
+turtle_story.goto(WINDOW_SIZE_X/2-450,WINDOW_SIZE_Y/2-40)
+turtle_story.write('story',font=("Arial",18,("bold","italic")))
 
 SQUARE_SIZE = 20
 catcher.goto(-(WINDOW_SIZE_X/2)+150,-(WINDOW_SIZE_Y/2)+90)
@@ -81,6 +85,16 @@ def left():
 turtle.onkeypress(right , RIGHT_ARROW)
 turtle.onkeypress(left , LEFT_ARROW)
 turtle.listen()
+
+def start():
+    if var==0:
+        global var
+        turtle_story.clear()
+        turtle_story.write('FOOD DROP!',font=("Arial",30,("bold","italic")))
+        var=1
+        falling_food()
+
+turtle.onkeypress(start,SPACEBAR)
 ##
 def move_catcher():
     global direction
@@ -96,6 +110,12 @@ def move_catcher():
         catcher.goto(x_pos - SQUARE_SIZE*3, y_pos)
         print("You moved left!")
 
+    if x_pos<=-250:
+       right()
+
+    elif x_pos>=250:
+       left()
+
 ##    my_pos=catcher.pos()
 ##    pos_list.append(my_pos)
 ##    new_stamp = catcher.stamp()
@@ -105,25 +125,28 @@ def move_catcher():
 ##    catcher.clearstamp(old_stamp)
 ##    pos_list.pop(0)
 ##
-
+food_type_list=['food-chicken.gif']#'food-chicken_nugets'#]
 food_list = []
 step = 25
 bottom = -WINDOW_SIZE_Y/2 + 50
 turtle.register_shape('food-chicken.gif')
+#turtle.register_shape('food-chicken_nugets.gif')
 def create_food():
     y_pos = WINDOW_SIZE_Y/2 - 50
     min_x = -int(WINDOW_SIZE_X/2/unitsize)+1
     max_x = int(WINDOW_SIZE_X/2/unitsize)-1
     x_pos = random.randint(min_x,max_x)*unitsize
     food = turtle.clone()
-    food.shape('food-chicken.gif')
+    chosen = random.randint(0,len(food_type_list)-1)
+    food.shape(food_type_list[chosen])
     food.goto(x_pos,y_pos)
     food.showturtle()
     food_list.append(food)
 food_delay = 0
 delay_num = 7
+    
 def falling_food():
-    global food_delay, m, n
+    global food_delay, m, n, delay_num
     food_destroy = []
     eaten_food = []
     for food in food_list:
@@ -134,7 +157,7 @@ def falling_food():
             food.goto(x_pos,y_pos)
             boat_x = catcher.pos()[0]
             boat_y = catcher.pos()[1]
-            if (x_pos >=  boat_x - 50 ) and (x_pos <=  boat_x + 50 ) and (y_pos >=  boat_y - 50 ) and (y_pos <=  boat_y + 50 ):
+            if (x_pos >=  boat_x - 50 ) and (x_pos <=  boat_x + 50 ) and (y_pos >=  boat_y - 50 ) and (y_pos <=  boat_y + 20 ):
                 ind = food_list.index(food)
                 eaten_food.append(ind)
         else:
@@ -156,6 +179,11 @@ def falling_food():
         n=n+1
         turtle_score.clear()
         turtle_score.write("score:"+str(n),font=("Arial",30,("bold","italic")) )
+        #after score=25 food falls faster
+        if n>=25:
+            delay_num = 4
+        if n>=40:
+            delay_num = 2
 
     if food_delay <= delay_num:
         food_delay += 1
@@ -170,6 +198,20 @@ def falling_food():
         
     turtle.ontimer(falling_food,100)
 
-falling_food()
+#falling_food()
+
+def create_trash():
+    y_pos = WINDOW_SIZE_Y/2 - 50
+    min_x = -int(WINDOW_SIZE_X/2/unitsize)+1
+    max_x = int(WINDOW_SIZE_X/2/unitsize)-1
+    x_pos = random.randint(min_x,max_x)*unitsize
+    trash = turtle.clone()
+    chosen = random.randint(0,len(food_type_list)-1)
+    trash.shape(trash_type_list[chosen])
+    trash.goto(x_pos,y_pos)
+    trash.showturtle()
+    trash_list.append(food)
+trash_delay = 0
+delay_num = 13
 
 
